877a9ec3a1c3cd98eeeb265152cf664ca27a280e
Elias Fatsi <efatsi@comcast.net>
2012-06-12 16:50:45 -0600

    rails app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HaxxorNews::Application.config.secret_token = '56b8cf532f5fac6c1159465a2bb8a7ec0d126d49fa501ab95024c9f407fbdbfb2813619a7d05c0f0061f92583ed1163154d139dad662d2c32a412aa5f8e172e3'

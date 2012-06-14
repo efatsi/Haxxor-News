4e0aebf0d24e2661fb33300d54ebfa772784c8f6
Elias Fatsi <efatsi@comcast.net>
2012-06-14 16:07:39 -0600

    times redone, about to mess with Gemfile so wanted to commit

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 HaxxorNews::Application.initialize!
+
+
+Time::DATE_FORMATS[:mdy] = "%b %d, %Y"
+Time::DATE_FORMATS[:mdyt] = "%b %d, %Y at %H:%M"

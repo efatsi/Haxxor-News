ace6fb5522a82798b85fce938af8540b690317eb
Elias Fatsi <efatsi@comcast.net>
2012-06-13 21:28:09 -0600

    changed user relation to articles in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ require 'faker'
 User.delete_all
 Article.delete_all
 
-User.create!(id: 1, username: "efatsi", role: "admin", password: "secret", password_confirmation: "secret")
-User.create!(id: 2, username: "afatsi", role: "user", password: "secret", password_confirmation: "secret")
+User.create!(username: "efatsi", role: "admin", password: "secret", password_confirmation: "secret")
+User.create!(username: "afatsi", role: "user", password: "secret", password_confirmation: "secret")
 
 50.times do |article|
-  Article.create!(title: Faker::Name.name, link: Faker::Internet.domain_name, user_id: 1, points: rand(100).to_i)
+  Article.create!(title: Faker::Name.name, link: Faker::Internet.domain_name, user_id: User.first.id, points: rand(100).to_i)
 end

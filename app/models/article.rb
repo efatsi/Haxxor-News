ccddb4eb576f8825b8fcf8097b7b293566547a43
Elias Fatsi <efatsi@comcast.net>
2012-06-15 16:25:42 -0600

    DS_Store file deleted

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ActiveRecord::Base
   delegate :username, :to => :user
   
   # before_validation :fill_user
+  before_validation :adjust_link
+  
   attr_accessible :link, :points, :title, :user_id
   
   # Relationships
@@ -11,6 +13,7 @@ class Article < ActiveRecord::Base
   
   # Validations
   validates_presence_of :title, :link, :user_id
+  validates_format_of :link, :with => /^(http|https):\/\/([a-z0-9]*[\-\.])?([a-z0-9]+*\.[a-z]{2,5})(:[0-9]{1,5})?(\/.*)?$/
   
   # Methods
   
@@ -32,6 +35,17 @@ class Article < ActiveRecord::Base
 	  end
 	  count
   end
-	
+  
+  def adjust_link
+    if self.link.match(/^(http|https):\/\/([a-z0-9]*[\-\.])?([a-z0-9]+*\.[a-z]{2,5})(:[0-9]{1,5})?(\/.*)?$/).nil? and !self.link.nil?
+      self.link = "http://" + self.link
+    end    
+  end
+  
+	def short_link
+	  if !link.match(/^(http|https):\/\/([a-z0-9]*[\-\.])?([a-z0-9]+*\.[a-z]{2,5})(:[0-9]{1,5})?(\/.*)?$/).nil? and !link.nil?
+	    $3.to_s unless $3.nil?
+    end
+  end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,22 @@
 require 'faker'
 
 
-User.delete_all
 Article.delete_all
+Comment.delete_all
 
 User.create!(username: "efatsi", role: "admin", password: "secret", password_confirmation: "secret")
-User.create!(username: "afatsi", role: "user", password: "secret", password_confirmation: "secret")
+User.create!(username: "amelia", role: "user", password: "secret", password_confirmation: "secret")
 
 50.times do |article|
-  Article.create!(title: Faker::Name.name, link: Faker::Internet.domain_name, user_id: User.first.id, points: rand(100).to_i)
+  if rand < 0.5
+    Article.create!(title: Faker::Name.name, link: "github.com/efatsi/Haxxor-News/pull/4/files", user_id: User.first.id, points: rand(100).to_i)
+  else
+    Article.create!(title: Faker::Name.name, link: "github.com/efatsi/Haxxor-News/pull/4/files", user_id: User.last.id, points: rand(100).to_i)
+  end    
 end
+
+Comment.create!(content: "Comment 1", commentable_id: Article.first.id, commentable_type: "Article", user_id: User.first.id)
+Comment.create!(content: "Comment 1.1", commentable_id: Comment.first.id, commentable_type: "Comment", user_id: User.first.id)
+Comment.create!(content: "Comment 1.2", commentable_id: Comment.first.id, commentable_type: "Comment", user_id: User.last.id)
+Comment.create!(content: "Comment 1.1.1", commentable_id: (Comment.first.id + 1), commentable_type: "Comment", user_id: User.first.id)
+Comment.create!(content: "Comment 2", commentable_id: Article.first.id, commentable_type: "Article", user_id: User.last.id)

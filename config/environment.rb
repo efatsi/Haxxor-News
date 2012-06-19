850a6848ad9424947eef8a7c5113ad9587243e17
Elias Fatsi <efatsi@comcast.net>
2012-06-19 12:03:19 -0600

    date formats organized in initializers

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 HaxxorNews::Application.initialize!
-
-
-Time::DATE_FORMATS[:mdy] = "%b %d, %Y"
-Time::DATE_FORMATS[:mdyt] = "%b %d, %Y at %H:%M"
-
-Rails.logger = Logger.new(STDOUT)

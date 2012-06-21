5108c265f982688e7d76af4b294172e3dd15ca48
Elias Fatsi <efatsi@comcast.net>
2012-06-21 09:06:05 -0600

    controller changes, redirects should avoid continuous loops now, and assigning seems unnecessary?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   before_filter :require_user, :except => [:new, :create]
-  skip_before_filter :store_location, :only => [:new, :create]
+  skip_before_filter :store_location, :only => [:new, :create, :edit, :update, :destroy]
 
   load_and_authorize_resource
   
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user
   end
   
   def index
@@ -18,7 +17,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
   
   def create
@@ -32,7 +30,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = current_user
     if @user.update_attributes(params[:user])
       redirect_to(@user, :notice => 'User was successfully updated.')
     else
@@ -41,7 +38,6 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
 
     respond_to do |format|

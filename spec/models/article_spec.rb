890a1c915f56df4ad969e5d1eb77af027755c44b
Elias Fatsi <efatsi@comcast.net>
2012-06-25 14:13:50 -0600

    spec tests for users, articles, and comments

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe Article do
+  
+  alpha = User.create(:username => "article_member", :password => "secret", :password_confirmation => "secret", :role => "admin")
+  google = Article.create(:link => "http://www.google.com", :title => "Google, Fake Article", :user_id => alpha.id)
+  
+  it "should validate a bunch of stuff" do
+    
+    should validate_presence_of(:link)
+    should validate_presence_of(:title)
+    should validate_presence_of(:user_id)
+    
+    should belong_to(:user)
+    should have_many(:comments)
+    should have_many(:votes)
+    
+    should allow_value("github.com").for(:link)
+    should allow_value("www.github.com").for(:link)
+    should allow_value("http://www.github.com").for(:link)
+    should_not allow_value(nil).for(:link)
+    should_not allow_value("").for(:link)
+    should_not allow_value("285").for(:link)
+    should_not allow_value("string").for(:link)
+    
+    should allow_value("This is an Article").for(:title)
+    should_not allow_value(nil).for(:title)
+    
+    should allow_value(1).for(:user_id)
+    should_not allow_value(nil).for(:user_id)
+  end  
+  
+  it "shows the article's attributes" do
+    assert google.save
+    assert_equal google.link, "http://www.google.com"
+    assert_equal google.title, "Google, Fake Article"
+  end
+  
+  it "should add http:// to link if necessary" do
+    walmart = Article.new(:link => "walmart.com", :title => "Walmart", :user_id => alpha.id)
+    target = Article.new(:link => "www.target.com", :title => "Target", :user_id => alpha.id)
+    assert walmart.save
+    assert target.save
+    assert_equal walmart.link, "http://walmart.com"
+    assert_equal target.link, "http://www.target.com"
+  end
+  
+  it "should show the short link" do
+    assert_equal google.short_link, "google.com"
+  end
+  
+  it "should increase the count by 1" do
+    current_count = google.comment_count
+    google.update_count
+    assert_equal google.comment_count, current_count+1
+  end
+  
+end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe Comment do
+  
+  alpha = User.create(:username => "comment_member", :password => "secret", :password_confirmation => "secret", :role => "admin")
+  google = Article.create(:link => "http://www.google.com", :title => "Google, Fake Article", :user_id => alpha.id)
+  c1 = Comment.create(:content => "1st Comment", :commentable_type => "Article", :commentable_id => google.id, :user_id => alpha.id)
+  
+  it "should validate a bunch of stuff" do
+    
+    should validate_presence_of(:content)
+    should validate_presence_of(:user_id)
+    should validate_presence_of(:commentable_id)
+    should validate_presence_of(:commentable_type)
+    
+    should belong_to(:user)
+    should belong_to(:commentable)
+    should have_many(:comments)
+    should have_many(:votes)
+    
+    should allow_value("This is a Comment!!!").for(:content)
+    should_not allow_value(nil).for(:content)
+    
+    should allow_value(1).for(:user_id)
+    should_not allow_value(nil).for(:user_id)
+    
+    should allow_value(1).for(:commentable_id)
+    should_not allow_value(nil).for(:commentable_id)
+    
+    should allow_value("Article").for(:commentable_type)
+    should allow_value("Comment").for(:commentable_type)
+    should_not allow_value(nil).for(:commentable_type)
+  end  
+  
+  it "should show the comment's attributes/parent" do
+    assert_equal c1.content, "1st Comment"
+    assert_equal c1.commentable_type, "Article"
+    assert_equal c1.commentable_id, google.id
+    assert_equal c1.user_id, alpha.id
+    assert_equal c1.commentable, google
+  end
+  
+  it "should increase count of itself and it's parent" do
+    current_c_count = c1.comment_count
+    current_p_count = c1.commentable.comment_count
+    c1.update_count
+    assert_equal current_c_count + 1, c1.comment_count
+    assert_equal current_p_count + 1, c1.commentable.comment_count
+  end
+  
+  
+  
+end

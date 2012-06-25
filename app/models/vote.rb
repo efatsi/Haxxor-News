2791a457f0df9873034141df8329e08f8f52067f
Elias Fatsi <efatsi@comcast.net>
2012-06-25 14:38:23 -0600

    voting specs and user-voting combo test written

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,7 @@ class Vote < ActiveRecord::Base
   attr_accessible :direction, :user_id, :votable_id, :votable_type
   
   validates_presence_of :direction, :user_id, :votable_id, :votable_type
+  validates_inclusion_of :direction, :in => ["up", "down"]
   
   belongs_to :user
   belongs_to :votable, :polymorphic => true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,4 +55,19 @@ describe User do
   it "should be able to get role of user" do
     assert alpha.role?(:admin)
   end
+  
+  it "should know if you've voted on something or not" do
+    delta = User.create(:username => "member3", :password => "secret", :password_confirmation => "secret", :role => "member")
+    
+    google = Article.create(:link => "http://www.google.com", :title => "Google, Fake Article", :user_id => alpha.id)
+    c1 = Comment.create(:content => "1st Comment", :commentable_type => "Article", :commentable_id => google.id, :user_id => alpha.id)
+    article_vote = Vote.create(:direction => "up", :votable_type => "Article", :votable_id => google.id, :user_id => alpha.id)
+    comment_vote = Vote.create(:direction => "up", :votable_type => "Comment", :votable_id => c1.id, :user_id => alpha.id)
+    
+    assert_equal alpha.voted_on(google), true
+    assert_equal alpha.voted_on(c1), true
+    assert_equal delta.voted_on(google), false
+    assert_equal delta.voted_on(c1), false
+  end
+  
 end

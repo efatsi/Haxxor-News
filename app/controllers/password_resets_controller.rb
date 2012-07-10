e17b862e05e1575f7d13d300e08339df1548c0dc
Elias Fatsi <efatsi@comcast.net>
2012-07-10 17:05:19 -0600

    cleaning up

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,34 +17,18 @@ class PasswordResetsController < ApplicationController
 
   def edit
     @password_reset = PasswordReset.find(params[:id])
-    
-    # @user = User.find_by_password_reset_token!(params[:id])
-    # @password_reset = PasswordReset.new(:user => @user)
     if @password_reset.expired?
       redirect_to new_password_reset_path, :alert => "Password reset has expired"
-    # else
-    #   log_user_in
     end
   end
 
   def update
-        @password_reset = PasswordReset.find(params[:id])
-    # @user = User.find_by_password_reset_token!(params[:id])
-    # @password_reset = PasswordReset.new(:user => @user)
-    
+    @password_reset = PasswordReset.find(params[:id])
     if @password_reset.update_attributes(params[:password_reset])
       redirect_to root_url, :notice => "Password has been reset, hurray!"
     else
-      # @password_reset.load_on_the_errors(params[:password_reset])
       render :edit
     end
     
   end
-  
-  # private
-  # 
-  # def log_user_in
-  #   cookies[:auth_token] = @user.auth_token
-  # end
-  
 end

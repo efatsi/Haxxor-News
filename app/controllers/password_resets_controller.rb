212e8718543f4c34b19c8cac20287dc391fbef6f
Elias Fatsi <efatsi@comcast.net>
2012-07-10 14:41:44 -0600

    cleaning up

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -36,14 +36,6 @@ class PasswordResetsController < ApplicationController
       render :edit
     end
     
-    # if @user.password_reset_sent_at < 2.hours.ago
-    #   redirect_to new_password_reset_path, :alert => "Password reset has expired."
-    # elsif @user.update_attributes(params[:user])
-    #   redirect_to root_url, :notice => "Password has been reset!"
-    # else
-    #   render :edit
-    # end
-    
   end
   
   private
